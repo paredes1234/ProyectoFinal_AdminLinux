ec21a404a2d2f7f591fafab5f3378c9d09bb526d
RodrigoFFN <rfloresnu@unsa.edu.pe>
2026-07-21 18:22:31 -0500

    Definir estructuras para representar archivos

---
+++ include/archivos.h
@@ -0,0 +1,23 @@
+#ifndef ARCHIVOS_H
+#define ARCHIVOS_H
+
+#include <limits.h>
+
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
+
+typedef struct {
+    char nombre[256];
+    char ruta[PATH_MAX];
+    int es_directorio;
+    long long tamano;
+} ItemArchivo;
+
+typedef struct {
+    ItemArchivo *items;
+    int total;
+    int truncada; /* 1 cuando la búsqueda alcanzó el límite de resultados */
+} ListaArchivos;
+
+#endif
