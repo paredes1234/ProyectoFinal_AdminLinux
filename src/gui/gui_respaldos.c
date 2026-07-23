8019a13f1ee16c56759d03149a2ddf1726d252d2
Fabricio Gabriel Jimenez Paredes <fjimenezp@unsa.edu.pe>
2026-07-23 17:23:20 -0500

    construir secciones de respaldos Bash y descargas GUI

--- src/gui/gui_respaldos.c
+++ src/gui/gui_respaldos.c
@@ -154,3 +154,135 @@ static void on_procesar_cola_clicked(GtkButton *btn, gpointer data) {
     gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(g_barra_progreso), 1.0);
     refrescar_cola_visual();
 }
+
+/* ---------------- Construcción de la pestaña ---------------- */
+
+static GtkWidget *crear_seccion_respaldos(void) {
+    GtkWidget *marco = gtk_frame_new("Respaldos incrementales");
+    GtkWidget *caja = gtk_box_new(GTK_ORIENTATION_VERTICAL, 6);
+    gtk_container_set_border_width(GTK_CONTAINER(caja), 10);
+
+    GtkWidget *fila1 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 6);
+    g_entrada_origen = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(g_entrada_origen), "Directorio origen");
+    gtk_widget_set_hexpand(g_entrada_origen, TRUE);
+    g_entrada_destino = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(g_entrada_destino), "Directorio de respaldos");
+    gtk_widget_set_hexpand(g_entrada_destino, TRUE);
+    GtkWidget *btn_crear = gtk_button_new_with_label("Crear respaldo");
+    gtk_box_pack_start(GTK_BOX(fila1), g_entrada_origen, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(fila1), g_entrada_destino, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(fila1), btn_crear, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(caja), fila1, FALSE, FALSE, 0);
+
+    GtkWidget *fila2 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 6);
+    GtkWidget *btn_listar_versiones = gtk_button_new_with_label("Listar versiones");
+    g_combo_versiones = gtk_combo_box_text_new();
+    gtk_widget_set_hexpand(g_combo_versiones, TRUE);
+    g_entrada_restauracion = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(g_entrada_restauracion), "Ruta donde restaurar");
+    gtk_widget_set_hexpand(g_entrada_restauracion, TRUE);
+    GtkWidget *btn_restaurar = gtk_button_new_with_label("Restaurar");
+    gtk_box_pack_start(GTK_BOX(fila2), btn_listar_versiones, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(fila2), g_combo_versiones, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(fila2), g_entrada_restauracion, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(fila2), btn_restaurar, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(caja), fila2, FALSE, FALSE, 0);
+
+    gtk_container_add(GTK_CONTAINER(marco), caja);
+
+    g_signal_connect(btn_crear, "clicked", G_CALLBACK(on_crear_respaldo_clicked), NULL);
+    g_signal_connect(btn_listar_versiones, "clicked", G_CALLBACK(on_listar_versiones_clicked), NULL);
+    g_signal_connect(btn_restaurar, "clicked", G_CALLBACK(on_restaurar_clicked), NULL);
+
+    return marco;
+}
+
+static GtkWidget *crear_seccion_bash(void) {
+    GtkWidget *marco = gtk_frame_new("Análisis de scripts Bash");
+    GtkWidget *caja = gtk_box_new(GTK_ORIENTATION_VERTICAL, 6);
+    gtk_container_set_border_width(GTK_CONTAINER(caja), 10);
+
+    GtkWidget *fila = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 6);
+    g_entrada_script = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(g_entrada_script), "Ruta del script .sh");
+    gtk_widget_set_hexpand(g_entrada_script, TRUE);
+    GtkWidget *btn_analizar = gtk_button_new_with_label("Analizar");
+    gtk_box_pack_start(GTK_BOX(fila), g_entrada_script, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(fila), btn_analizar, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(caja), fila, FALSE, FALSE, 0);
+
+    GtkWidget *scroll = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+    gtk_widget_set_size_request(scroll, -1, 160);
+    GtkWidget *vista = gtk_text_view_new();
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(vista), FALSE);
+    gtk_text_view_set_monospace(GTK_TEXT_VIEW(vista), TRUE);
+    g_buffer_bash = gtk_text_view_get_buffer(GTK_TEXT_VIEW(vista));
+    gtk_container_add(GTK_CONTAINER(scroll), vista);
+    gtk_box_pack_start(GTK_BOX(caja), scroll, TRUE, TRUE, 0);
+
+    gtk_container_add(GTK_CONTAINER(marco), caja);
+    g_signal_connect(btn_analizar, "clicked", G_CALLBACK(on_analizar_bash_clicked), NULL);
+    return marco;
+}
+
+static GtkWidget *crear_seccion_cola(void) {
+    GtkWidget *marco = gtk_frame_new("Cola de descargas");
+    GtkWidget *caja = gtk_box_new(GTK_ORIENTATION_VERTICAL, 6);
+    gtk_container_set_border_width(GTK_CONTAINER(caja), 10);
+
+    GtkWidget *fila = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 6);
+    g_entrada_cola = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(g_entrada_cola), "URL o ruta a encolar");
+    gtk_widget_set_hexpand(g_entrada_cola, TRUE);
+    GtkWidget *btn_agregar = gtk_button_new_with_label("Agregar");
+    GtkWidget *btn_procesar = gtk_button_new_with_label("Procesar cola");
+    gtk_box_pack_start(GTK_BOX(fila), g_entrada_cola, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(fila), btn_agregar, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(fila), btn_procesar, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(caja), fila, FALSE, FALSE, 0);
+
+    g_store_cola = gtk_list_store_new(N_COLUMNAS_COLA, G_TYPE_STRING);
+    g_treeview_cola = gtk_tree_view_new_with_model(GTK_TREE_MODEL(g_store_cola));
+    GtkCellRenderer *renderer = gtk_cell_renderer_text_new();
+    GtkTreeViewColumn *columna = gtk_tree_view_column_new_with_attributes(
+        "Elemento", renderer, "text", COL_COLA_ITEM, NULL);
+    gtk_tree_view_append_column(GTK_TREE_VIEW(g_treeview_cola), columna);
+
+    GtkWidget *scroll = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+    gtk_widget_set_size_request(scroll, -1, 120);
+    gtk_container_add(GTK_CONTAINER(scroll), g_treeview_cola);
+    gtk_box_pack_start(GTK_BOX(caja), scroll, TRUE, TRUE, 0);
+
+    g_barra_progreso = gtk_progress_bar_new();
+    gtk_progress_bar_set_show_text(GTK_PROGRESS_BAR(g_barra_progreso), TRUE);
+    gtk_box_pack_start(GTK_BOX(caja), g_barra_progreso, FALSE, FALSE, 0);
+
+    gtk_container_add(GTK_CONTAINER(marco), caja);
+
+    g_signal_connect(btn_agregar, "clicked", G_CALLBACK(on_agregar_cola_clicked), NULL);
+    g_signal_connect(g_entrada_cola, "activate", G_CALLBACK(on_agregar_cola_clicked), NULL);
+    g_signal_connect(btn_procesar, "clicked", G_CALLBACK(on_procesar_cola_clicked), NULL);
+
+    return marco;
+}
+
+GtkWidget *gui_respaldos_crear_tab(GtkWidget *ventana_principal) {
+    g_ventana = ventana_principal;
+
+    GtkWidget *scroll_general = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll_general),
+        GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
+
+    GtkWidget *caja = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_container_set_border_width(GTK_CONTAINER(caja), 10);
+
+    gtk_box_pack_start(GTK_BOX(caja), crear_seccion_respaldos(), FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(caja), crear_seccion_bash(), TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(caja), crear_seccion_cola(), FALSE, FALSE, 0);
+
+    gtk_container_add(GTK_CONTAINER(scroll_general), caja);
+    return scroll_general;
+}
